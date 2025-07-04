4d3e8586826d36272929dfba7d83a2651607053d
Tohavina RAMANANJATOVO <tohavinarama@gmail.com>
2025-07-04 10:35:58 +0200

    déploiement

--- forumSTM/migrations/Version20250704081623.php
+++ forumSTM/migrations/Version20250704081623.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250703155645 extends AbstractMigration
+final class Version20250704081623 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,13 +21,13 @@ final class Version20250703155645 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE comment ADD posts_id INT DEFAULT NULL
+            ALTER TABLE category_posts DROP FOREIGN KEY FK_606EF5B3D5E258C5
         SQL);
         $this->addSql(<<<'SQL'
-            ALTER TABLE comment ADD CONSTRAINT FK_9474526CD5E258C5 FOREIGN KEY (posts_id) REFERENCES posts (id)
+            ALTER TABLE category_posts DROP FOREIGN KEY FK_606EF5B312469DE2
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE INDEX IDX_9474526CD5E258C5 ON comment (posts_id)
+            DROP TABLE category_posts
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE user CHANGE roles roles JSON NOT NULL
@@ -41,13 +41,13 @@ final class Version20250703155645 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE comment DROP FOREIGN KEY FK_9474526CD5E258C5
+            CREATE TABLE category_posts (category_id INT NOT NULL, posts_id INT NOT NULL, INDEX IDX_606EF5B3D5E258C5 (posts_id), INDEX IDX_606EF5B312469DE2 (category_id), PRIMARY KEY(category_id, posts_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = '' 
         SQL);
         $this->addSql(<<<'SQL'
-            DROP INDEX IDX_9474526CD5E258C5 ON comment
+            ALTER TABLE category_posts ADD CONSTRAINT FK_606EF5B3D5E258C5 FOREIGN KEY (posts_id) REFERENCES posts (id) ON DELETE CASCADE
         SQL);
         $this->addSql(<<<'SQL'
-            ALTER TABLE comment DROP posts_id
+            ALTER TABLE category_posts ADD CONSTRAINT FK_606EF5B312469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT 'NULL' COMMENT '(DC2Type:datetime_immutable)'
